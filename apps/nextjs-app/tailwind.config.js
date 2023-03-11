cabe43da3a874e2cb0c305bfe531c93bc73b2143
ndhungw <ndh1379@gmail.com>
2023-03-11 09:29:40 +0700

    setup tailwind presets

--- apps/nextjs-app/tailwind.config.js
+++ apps/nextjs-app/tailwind.config.js
@@ -1,13 +1,10 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
-const { join } = require('path');
+const { createGlobPatternsForDependencies } = require("@nrwl/react/tailwind");
+const { join } = require("path");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(
-      __dirname,
-      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
+    join(__dirname, "{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}"),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
