5872e7e7091b7b8bb90fd2069fbbc29577005439
ndhungw <ndh1379@gmail.com>
2023-03-11 09:21:22 +0700

    share prettier configs across projects

--- libs/shared/ui/prettier.config.js
+++ libs/shared/ui/prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    plugins: [require('prettier-plugin-tailwindcss')],
-    tailwindConfig: './tailwind.config.js',
-  };
-  
+  ...require("./../../../.prettierrc.js"),
+  plugins: [require("prettier-plugin-tailwindcss")],
+  tailwindConfig: "./tailwind.config.js",
+};
